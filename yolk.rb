1bf23777bfe56e2e7329f00ea0f3c9af27ce15d6
mezzo <mezzo@neonnoon.ch>
2018-08-26 10:00:44 +0200

    Converted release number for yolk.

--- yolk.rb
+++ yolk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Yolk < Formula
   homepage 'https://github.com/neonnoon/yolk'
-  url 'https://github.com/neonnoon/yolk/archive/0.1.0.tar.gz'
-  sha256 '26706c7efcd867859d73251ff8d206988dfc588925bfb4e656e9cd3b14c17385'
+  url 'https://github.com/neonnoon/yolk/archive/v0.1.0.tar.gz'
+  sha256 'd80a434fd3fe455c79af31aedd4f50454456f85369aaedcf7f6af49c536ff89b'
 
   head 'https://github.com/neonnoon/yolk.git'
 
